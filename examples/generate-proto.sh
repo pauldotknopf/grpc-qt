7b42f584a5a46c98720c83ece9df5bb0da231dcb
Paul Knopf <pauldotknopf@gmail.com>
2020-02-12 13:38:12 -0500

    Initial commit.

---
+++ examples/generate-proto.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+protoc --cpp_out=./client/proto \
+    --grpc_out=./client/proto \
+    --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` \
+    -I./proto \
+    types.proto
+protoc --cpp_out=./client/proto \
+    --grpc_out=./client/proto \
+    --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` \
+    -I./proto \
+    gen.proto
---
+++ src/GrpcQt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Google.Protobuf;
+using Google.Protobuf.Compiler;
+
+namespace GrpcQt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (!Debugger.IsAttached)
+                {
+                    Thread.Sleep(1000);
+                }
+                Debugger.Break();
+
+                using (var stdin = Console.OpenStandardInput())
+                using (var stdout = Console.OpenStandardOutput())
+                {
+                    var request = CodeGeneratorRequest.Parser.ParseFrom(stdin);
+                    var response = new CodeGeneratorResponse();
+                    response.WriteTo(new CodedOutputStream(stdout, true));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
